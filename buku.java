29f2c7c892989119ae0fa3346abc312a8b07b6ae
Panjisetiadarma <setiadarmapanji@gmail.com>
2025-10-23 08:24:01 +0700

    contoh

---
+++ buku.java
@@ -0,0 +1,18 @@
+public class buku {
+    String judul;
+    String penulis;
+    int tahunTerbit;
+
+    public buku(String judul, String penulis, int tahunTerbit) {
+        this.judul = judul;
+        this.penulis = penulis;
+        this.tahunTerbit = tahunTerbit;
+    }
+
+    public void tampilkanInfo() {
+        System.out.println("Judul: " + judul);
+        System.out.println("Penulis: " + penulis);
+        System.out.println("Tahun Terbit: " + tahunTerbit);
+    }
+    
+}
